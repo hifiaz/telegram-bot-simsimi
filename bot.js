5f959d7282504ccb6858b1b9d10229d564c24891
Kucekmill <79855424+kucekmill@users.noreply.github.com>
2021-07-18 14:34:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 
-const token = '546288950:AAEJiCPtJoMxK-1zZJnbILVNpsSJ6-20ZAs';
-const simsimiKey = 'bbbcffc4-9c64-4427-8916-1ee9fe249f16';
+const token = '1900074132:AAEqapGqGYXKy7q-7k2bg585DczlypZpQ5w';
+const simsimiKey = 'mqm-EbYtL~UELX5K__mh0vQc6FKqIOtX2SKfK2U7';
 const url = `http://sandbox.api.simsimi.com/request.p?key=${simsimiKey}&lc=id&ft=1.0&text=`
 
 const bot = new TelegramBot(token, {polling: true});
